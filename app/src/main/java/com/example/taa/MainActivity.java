05321a0b5ce0932189ea770e93ffef59a36a40b8
Giner Figueroa Gonzalez <2134132@untels.edu.pe>
2024-07-21 17:18:03 -0500

    Agregando primer archivo 1

---
+++ app/src/main/java/com/example/taa/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.taa;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+      Button btnLogin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnLogin=findViewById(R.id.button);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,NotaActivity.class );
+                startActivity(i);
+            }
+        });
+    }
+}
